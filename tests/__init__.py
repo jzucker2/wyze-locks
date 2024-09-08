41c710b31d4e9e328b3004c4c4c3a4b01d798987
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-08 12:17:04 -0700

    Starting the project

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Wyze Locks integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Wyze Locks tests."""
+from custom_components.wyze_locks.const import (
+    CONF_PASSWORD,
+)
+from custom_components.wyze_locks.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
